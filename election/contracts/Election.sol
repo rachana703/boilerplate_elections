999c46cb8e2927ee34bfa3a36b777ecb792fe5a5
rachana703 <rachana.gandhi@somaiya.edu>
2018-06-30 11:08:10 +0530

    changes done

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -23,27 +23,31 @@ contract Election {
     );
 
     function Election () public {
-    // call addCandidate() here to add candidates as we dont want anyone else to do this, smart contract is incharge of how many candidates exist
-      
+	// call addCandidate() here to add candidates as we dont want anyone else to do this, smart contract is incharge of how many candidates exist
+      /* write your code here */
     }
 
     function addCandidate (string _name) private {
-    // Add new candidate each time this function ls called
-    // First increment the candidate count
-    // Assign candidate values to candidates with reference to key which is our candidate count 
-      
+	// Add new candidate each time this function ls called
+	// First increment the candidate count
+	// Assign candidate values to candidates with reference to key which is our candidate count 
+      /* write your code here */
     }
 
     function vote (uint _candidateId) public {
         // require that they haven't voted before
-    
+       /* write your code here */
+
         // require a valid candidate
+       /* write your code here */
 
         // record that voter has voted
-        
+        /* write your code here */
+
         // update candidate vote Count
-      
+      /* write your code here */
+
         // trigger voted event
-      
+       /* write your code here */
     }
 }
